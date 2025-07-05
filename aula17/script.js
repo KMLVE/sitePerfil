41ce834ef28768907e470f67a3f910b2dccfd7af
KMLVE <marquessouzakeila@gmail.com>
2025-07-05 15:34:02 -0300

    Add files via upload

---
+++ aula17/script.js
@@ -0,0 +1,33 @@
+let link_1 = "https://www.instagram.com/"
+let link_2 = "https://github.com/KMLVE"
+let link_3 = "https://www.glassdoor.com.br/index.htm"
+let link_4 = "https://www.catho.com.br/"
+
+
+function direcionar(meuLink) {
+    
+    if(meuLink == 1){
+
+        window.location.href = link_1
+    }
+
+
+    if(meuLink == 2){
+
+        window.location.href = link_2
+    }
+
+
+    if(meuLink == 3){
+
+        window.location.href = link_3
+    }
+
+
+    if(meuLink == 4){
+
+        window.location.href = link_4
+    }
+}
+
+
